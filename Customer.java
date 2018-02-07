701f172e691df040c8a3b7f0097549d867f82949
Mattt-K <35709212+Mattt-K@users.noreply.github.com>
2018-02-07 09:09:31 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,16 +1,21 @@
 /** This class creates a customer that has a numerical ID and a name. This class can be called externally for use in identifying 
 individuals and relating them to a BankAccount */
 public class Customer{
+    
+    // Global variables set to default values.
     String name;
     int customerID;
     
+    /* This is a default constructor */
     public Customer(){}
     
+    /* This is a constructor that can set a new name and customer ID */
     public Customer(String name, int customerID){
         this.name = name;
         this.customerID = customerID;
     }
     
+    /*
     public Customer(Customer something){
         name = something.name;
         customerID = something.customerID;
