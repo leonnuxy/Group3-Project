e5b65984a835e88e704d05da08155281d46c57a4
leonnuxy <35940208+leonnuxy@users.noreply.github.com>
2018-02-11 17:55:12 -0700

    Add files via upload

--- BankAccount.java
+++ BankAccount.java
@@ -1,54 +1,55 @@
 /** This class checks that a deposit is not negative and that any withdrawl does not exceed a pre set overdraft. This class can be called externally for use in checking fund transfer information */
 public class BankAccount {
-    
+
     // Global variables set to the default values.
     double balance;
     double overdraftAmount = 100.0;
     Customer c;
-    
+
     /* This is a default constructor */
-    public BankAccount(){}
-    
+    public BankAccount(Customer customer){
+      this.c = customer;
+    }
+
     /* This method creates a constructor that takes the values for a customer and a new balance for the bank account */
     public BankAccount(Customer aCustomer, double balance){
         this.balance = balance;
         this.c = aCustomer;
-        
+
     }
-    
+
     /* This method is a getter that returns the customer information */
     public Customer getCustomer() {
         return c;
     }
-    
+
     /* This method is a setter that sets the name of the customer */
     public void  setCustomer(Customer someCustomer){
         c = someCustomer;
     }
-    
+
     /* This method checks that the deposited amount is greater than 0 and adds to their total balance */
     public void deposit(double deposit_amount) {
         if (deposit_amount >= 0){
             balance = balance + deposit_amount;
         }
     }
-    
+
     /* This method ensures that the user is not withdrawing more than their specified overdraft amount and calculates their total balance.*/
     public void withdraw(double withdraw_amount) {
         if (withdraw_amount - balance < overdraftAmount) {
             balance = balance - withdraw_amount;
         }
     }
-    
+
     /* This getter method returns the balance */
     public double getBalance() {
         return balance;
     }
-    
+
     /* This setter method allows the overdraft amount to be specified externally and returns the overdraftAmount after its assigned to the instance variable.*/
     public double setOverdraftAmount(double amount) {
         overdraftAmount = amount;
         return overdraftAmount;
     }
 }
-
--- BankAccountWindow.java
+++ BankAccountWindow.java
@@ -10,78 +10,99 @@ import javafx.geometry.Insets;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
+// Creates the class for with the application is named.
 public class BankAccountWindow extends Application {
-    Customer c1 = new Customer();
-    BankAccount b1 = new BankAccount(c1);
-    Button withdraw_button;
-    Button deposit_button;
-    TextField txtWithdraw, txtDeposit;
-    Label customer_balance;
-    
-    /**public static void main(String[] args) {
-        launch(args);
-    }*/
-    
-    
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        
-        BorderPane root = new BorderPane();
-        root.setPadding(new Insets(10,10,10,10));
-        
+
+// contsructor to refrence the customer from the
+// Customer class and assigns to the variable "c1".
+  Customer c1 = new Customer();
+
+// contsructor to refrence the information of
+//the customer from the BankAccount class which also references
+//the Customer class within it and assigns it to the varaible b.
+  BankAccount b1 = new BankAccount(c1);
+
+  //creates required buttons, labels and textfeilds.
+  Button withdraw_button;
+  Button deposit_button;
+  TextField txtWithdraw, txtDeposit;
+  Label customer_balance;
+
+  @Override
+  //creates the stage/window and adds the labels and appropriate items.
+  public void start(Stage primaryStage) throws Exception {
+
+    // creates the borders and sets the areas.
+    BorderPane root = new BorderPane();
+    root.setPadding(new Insets(10,10,10,10));
+
+    // displayes the balance of customer
+    double bank_balance = b1.getBalance();
+    root.setTop(new Label("Hello "+ c1.getName()+ " Deposit and Withdraw from Account"));
+    customer_balance = new Label("");
+    root.setBottom(customer_balance);
+    customer_balance.setText("Your balance is " + bank_balance);
+
+    // defines the size of pane.
+    GridPane center = new GridPane();
+    center.setVgap(10);
+    center.setHgap(5);
+
+    // sets the layouts for buttons and textfieldss.
+    withdraw_button = new Button("Withdraw");
+    center.add(withdraw_button, 0, 0);
+
+    // for deposit button
+    deposit_button = new Button("Deposit");
+    center.add(deposit_button, 0, 1);
+
+    // for withdraw button.
+    txtWithdraw = new TextField("0");
+    txtWithdraw.setPrefWidth(100);
+
+    // for deposit textfield box
+    txtDeposit = new TextField("0");
+    txtDeposit.setPrefWidth(100);
+
+    // for withdraw textfield box.
+    center.add(txtWithdraw, 1, 0);
+    center.add(txtDeposit, 1, 1);
+    root.setCenter(center);
+
+    // method for handling "deposit" inputs.
+    deposit_button.setOnAction(new EventHandler<ActionEvent>()
+    {
+      @Override
+      // handles the input and key actions.
+      // prints the current balance of the customer after each deposit
+      public void handle(ActionEvent event) {
+        double deposit_amount = Double.parseDouble(txtDeposit.getText());
+        b1.deposit(deposit_amount);
         double bank_balance = b1.getBalance();
-        root.setTop(new Label("For depositing and withdrawing money from your account: " + c1.getName()));
-        
-        customer_balance = new Label("");
-        root.setBottom(customer_balance);
         customer_balance.setText("Your balance is " + bank_balance);
-        
-        GridPane center = new GridPane();
-        center.setVgap(10);
-        center.setHgap(5);
-        withdraw_button = new Button("Withdraw");
-        center.add(withdraw_button, 0, 0);
-        deposit_button = new Button("Deposit");
-        center.add(deposit_button, 0, 1);
-        txtWithdraw = new TextField("0");
-        txtWithdraw.setPrefWidth(100);
-        txtDeposit = new TextField("0");
-        txtDeposit.setPrefWidth(100);
-        center.add(txtWithdraw, 1, 0);
-        center.add(txtDeposit, 1, 1);
-        root.setCenter(center);
-        
-        deposit_button.setOnAction(new EventHandler<ActionEvent>()
-        
-        {
-            @Override
-            public void handle(ActionEvent event) {
-                double deposit_amount = Double.parseDouble(txtDeposit.getText());
-                b1.deposit(deposit_amount);
-                double bank_balance = b1.getBalance();
-                customer_balance.setText("Your balance is " + bank_balance);
-                
-            }
-        }
-        );
-        
-        withdraw_button.setOnAction(new EventHandler<ActionEvent>()
-        {
-            @Override
-            public void handle(ActionEvent event) {
-                double withdraw_amount = Double.parseDouble(txtWithdraw.getText());
-                b1.withdraw(withdraw_amount);
-                double bank_balance = b1.getBalance();
-                customer_balance.setText("Your balance is " + bank_balance);
-            }
-        }
-        );
-        
-        
-        Scene scene = new Scene(root, 450, 150);
-        primaryStage.setTitle("Bank Account");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+      }
     }
-    
+    );
+
+    // method for handling "withdraw" inputs.
+    withdraw_button.setOnAction(new EventHandler<ActionEvent>()
+    {
+      // defines the input and key events.
+      // prints the current balance of the customer after each withdrawal
+      @Override
+      public void handle(ActionEvent event) {
+        double withdraw_amount = Double.parseDouble(txtWithdraw.getText());
+        b1.withdraw(withdraw_amount);
+        double bank_balance = b1.getBalance();
+        customer_balance.setText("Your balance is " + bank_balance);
+      }
+    }
+    );
+
+    //Sets the title of the popup window "Group3 Financial Bank".
+    Scene scene = new Scene(root, 450, 150);
+    primaryStage.setTitle("Group3 Financial Bank");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
 }
