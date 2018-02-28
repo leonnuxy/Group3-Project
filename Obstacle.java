73880af736a1a07fa70d18e3925909b5c1072080
nolaptopnolife <36118607+nolaptopnolife@users.noreply.github.com>
2018-02-28 09:39:39 -0700

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -1,10 +1,11 @@
 import java.util.Random;
 
+/** This class creates an Obstacle */
 public class Obstacle {
 
 int obsXPos;
 int obsYPos;
-int obsWidth;
+int	obsWidth;
 int obsHeight;
 
 Random rand = new Random();
@@ -17,23 +18,23 @@ public Obstacle(int borX, int borY, int borW, int borH){
 	this.obsHeight = borH;
 }
 
-public int setXPos(int eraseX){
-	obsXPos = rand.nextInt(50) + 1;
+public int setXPos(){
+	obsXPos = rand.nextInt(300) + 1;
 	return obsXPos;
 }
 
-public int setYPos(int eraseY){
-	obsYPos = rand.nextInt(50) + 1;
+public int setYPos(){
+	obsYPos = rand.nextInt(300) + 1;
 	return obsYPos;
 }
 
-public int setWidth(int eraseW){
-	obsWidth = rand.nextInt(30) + 1;
+public int setWidth(){
+	obsWidth = rand.nextInt(50) + 1;		
 	return obsWidth;
 }
 
-public int setHeight(int eraseH){
-	obsHeight = rand.nextInt(30) + 1;
+public int setHeight(){
+	obsHeight = rand.nextInt(50) + 1;	
 	return obsHeight;
 }
 
@@ -42,7 +43,7 @@ public int setHeight(int eraseH){
 //implementation using graphicscontext in the field: 
 //Obstacle obs1 = new Obstacle();
 //strokeRect(obs1.setYPos(),obs1.setYPos(), obs1.setWidth(), obs1.setHeight());
-//Collectible will work the same but with fixed height and width.
+//Collectible will work the same but with fixed height and width and a delete method.
 
 
 }
