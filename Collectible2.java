e7ecc94dc144e09dd30efa81d53af30621539cae
Mattt-K <35709212+Mattt-K@users.noreply.github.com>
2018-03-02 10:09:26 -0700

    Add files via upload

--- Collectible2.java
+++ Collectible2.java
@@ -1,12 +1,42 @@
-/** This has the properties of the collectible */
-public class Collectible2 {
-    private static char collectibleChar = 'c';
-    
-    public static void placeCollectible(){
-        char[][] game_space_collectible = GameSpace.getGameSpace();
-        game_space_collectible[3][4] = collectibleChar;
-        
-        GameSpace.setGameSpace(game_space_collectible);
-    }
-}
+import java.util.Random;
 
+/** This class creates an Obstacle */
+public class Collectible2 {
+    
+    static int colXPos, xDefine2;
+    static int colYPos, yDefine2;
+    int    colWidth = 15;
+    int colHeight = 15;
+    
+    static Random rand = new Random();
+    
+    public Collectible2(){
+    }
+    
+    //If want to purposefully place collectible
+    public Collectible2(int arbtyX, int arbtyY){
+        this.colXPos = arbtyX;
+        this.colYPos = arbtyY;
+        
+    }
+    
+    public static int xPos2(){
+        colXPos = rand.nextInt(490) + 1;
+        return colXPos;
+    }
+    
+    public static int yPos2(){
+        colYPos = rand.nextInt(490) + 1;
+        return colYPos;
+    }
+    
+    public int setWidth2(){
+        return colWidth;
+    }
+    
+    public int setHeight2(){
+        return colHeight;
+    }
+    
+    
+}
