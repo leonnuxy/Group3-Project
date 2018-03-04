d19ce36f17a887837990abbf9bef5ff2831793d8
leonnuxy <35940208+leonnuxy@users.noreply.github.com>
2018-03-03 17:52:55 -0700

    Add files via upload

--- Collectible.java
+++ Collectible.java
@@ -1,91 +1,48 @@
 import java.util.Random;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
 
-/** This class creates a collectible */
-public class Collectible {
+/** This class creates an Obstacle */
+public class Collectible{
 
-int colXPos, xDefine2;
-int colYPos, yDefine2;
-int	colWidth = 10;
-int colHeight = 10;
-Rectangle colWindow;
+    static int colXPos, xDefine2;
+    static int colYPos, yDefine2;
+    int    colWidth = 15;
+    int colHeight = 15;
 
-Random rand = new Random();
+    static Random rand = new Random();
 
-public Collectible(){
-	this.colXPos = this.setXPos2();
-	this.colYPos = this.setYPos2();
-	this.colWidth = this.setWidth2();
-	this.colHeight = this.setHeight2();
-	this.colWindow = this.getCol();
+    public Collectible(){}
 
-}
+    //If want to purposefully place collectible
+    public Collectible(int arbtyX, int arbtyY){
+        this.colXPos = arbtyX;
+        this.colYPos = arbtyY;
 
-//If want to purposefully place collectible
-public Collectible(int arbtyX, int arbtyY){
-	this.colXPos = arbtyX;
-	this.colYPos = arbtyY;
+    }
 
-}
+    public static int xPos2(){
+        colXPos = rand.nextInt(490) + 1;
+        return colXPos;
+    }
 
-public int setXPos2(){
-xDefine2 = rand.nextInt(6) + 1;
-	
-	if (xDefine2 == 1) {
-	colXPos = rand.nextInt(10) + 5;
-	}
-	if (xDefine2 == 2) {
-	colXPos = rand.nextInt(100-90) + 90;
-	}
-	if (xDefine2 == 3) {
-	colXPos = rand.nextInt(200-190) + 190;
-	}
-	if (xDefine2 == 4) {
-	colXPos = rand.nextInt(300-290) + 290;
-	}
-	if (xDefine2 == 5) {
-	colXPos = rand.nextInt(400-390) + 390;
-	}
-	if (xDefine2 == 6) {
-	colXPos = rand.nextInt(500-490) + 490;
-	}
-	return colXPos;
-}
+    public static int yPos2(){
+        colYPos = rand.nextInt(490) + 1;
+        return colYPos;
+    }
 
-public int setYPos2(){
-yDefine2 = rand.nextInt(5) + 1;
-	
-	if (xDefine2 == 1) {
-	colYPos = rand.nextInt(10) + 5;
-	}
-	if (yDefine2 == 2) {
-	colYPos = rand.nextInt(100-90) + 90;
-	}
-	if (yDefine2 == 3) {
-	colYPos = rand.nextInt(200-190) + 190;
-	}
-	if (yDefine2 == 4) {
-	colYPos = rand.nextInt(300-290) + 290;
-	}
-	if (yDefine2 == 5) {
-	colYPos = rand.nextInt(400-390) + 390;
-	}
-	if (yDefine2 == 6) {
-	colYPos = rand.nextInt(500-490) + 490;
-	}
-	return colYPos;
-}
+    public int setWidth2(){
+        return (colWidth = 20);
+    }
 
-public int setWidth2(){
-	return colWidth;
-}
+    public int setHeight2(){
+        return (colHeight = 20);
+    }
 
-public int setHeight2(){
-	return colHeight;
-}
+    public Rectangle collectible(){
+      Rectangle collectibles = new Rectangle(xPos2(), yPos2(), setWidth2(), setHeight2());
+      collectibles.setFill(Color.BLUE);
+      return collectibles;
+    }
 
-public Rectangle getCol(){
-	colWindow = new Rectangle(colXPos, colYPos, colWidth, colHeight);
-	return colWindow;
-}
 }
