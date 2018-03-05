e208212acdb3b89036a477947d6459646244a1fb
Mattt-K <35709212+Mattt-K@users.noreply.github.com>
2018-03-04 20:23:56 -0700

    Update Avatar_Jon.java

--- Avatar_Jon.java
+++ Avatar_Jon.java
@@ -1,11 +1,14 @@
 import javafx.scene.shape.Rectangle;
 
+/**
+This class is responsible for initializing the rectangle that is used for a player movable object
+*/
 public class Avatar_Jon{
 
   //Constructor for the class
   public Avatar_Jon(){}
 
-  // function to draw the rectangle
+  //Draws the rectangle to be used
   public Rectangle avatar_Snake(int x, int y){
     Rectangle player_Ava =new Rectangle(x, y, 10, 10);
     return player_Ava;
