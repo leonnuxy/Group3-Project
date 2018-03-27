83779f60a0e2b6d5cd761089acc4be8009e7b4be
leonnuxy <35940208+leonnuxy@users.noreply.github.com>
2018-03-27 15:21:36 -0600

    Add files via upload

--- BankAccountApp.java
+++ BankAccountApp.java
@@ -29,6 +29,7 @@ public class BankAccountApp extends Application {
 	static File file = new File(file_name);
 	public static boolean check;
 	
+	//Instances referencing the Objects in BankAccount through SavingsAccount and ChequingAccount
 	static BankAccount savings = new SavingsAccount();
 	static BankAccount chequing = new ChequingAccount();
 	static Customer customer;
@@ -39,24 +40,23 @@ public class BankAccountApp extends Application {
 	TextField txtWithdraw, txtDeposit;
 	
 	static TextField txtName;
-	
 	static int customer_ID;
-	static double balance;
+	static double balance, balz;
 	static String accountType = "";
 	static String ID = "";
-	static String name, cust_name;
+	static String name, cust_name, bal;
 	Random rand = new Random();
 	
+	//Checks if file exists in folder.
 	public static boolean file_check() {
 		if (file.exists()) {
 			check = true;
-		}
-		return check;
+		}return check;
 	}
 	
 	// Reads the file if the Bank Text already exists
 	public static void read_file(){
-		String id, bal;
+		String id;
 		
 		try {
 			FileReader file_r = new FileReader(file_name);
@@ -78,12 +78,12 @@ public class BankAccountApp extends Application {
 			
 			// Checks the type of account
 			if (accountType.contains("ChequingAccount")) {
-				chequing.deposit(balance);
+				chequing.setBalance(balance);
 			}
 			if (accountType.contains("SavingsAccount")) {
-    	    	savings.deposit(balance);
+    	    	savings.setBalance(balance);
     	    }
-			
+			//Get rid of this Meant for Tracing.
 			System.out.println(accountType);
 			System.out.println(customer.getName());
 			System.out.println(customer.getID());
@@ -93,25 +93,25 @@ public class BankAccountApp extends Application {
 			//close file
 			file_out.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			System.out.println("File not found creating a new file.");
-			
 		}
 		catch (IOException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
+			System.out.println("Somthing is Wrong: Diagnose");
+		}
+		catch (Exception e) {
+			//e.printStackTrace();
+			System.out.println("Somthing is Wrong: Diagnose");
 		}
 	}
-
 	
+	//Displays the layouts and windows and scenes.
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
-		//New account creation
+		//New account creation: First Screen
 	    BorderPane root = new BorderPane();
 	    root.setPadding(new Insets(10,10,10,10));
 	    root.setTop(new Label("Please select an account to create: "));
-	    
-	    // Choice: First Screen
 	    layout1 = new GridPane();
 	    layout1.setVgap(10);
 	    layout1.setHgap(5);
@@ -129,6 +129,7 @@ public class BankAccountApp extends Application {
 	    customer_ID_label = new Label("Customer ID is: " + customer_ID);
 	    txtName = new TextField("Enter name here");
 	    submit = new Button("Sumbit");
+	    acctType_label = new Label(""+accountType+"");
 	    layout2.add(customer_ID_label, 0, 1);
 	    layout2.add(txtName, 0, 2);
 	    layout2.add(submit, 0, 3);
@@ -137,12 +138,8 @@ public class BankAccountApp extends Application {
 	    layout3 = new GridPane();
 	    layout3.setVgap(10);
 	    layout3.setHgap(5);
-	    if (accountType.contains("ChequingAccount")) {
-	    	balance = chequing.getBalance();
-	    }
-	    if (accountType.contains("SavingsAccount")) {
-	    	balance = savings.getBalance();
-	    }
+	    if (accountType.contains("ChequingAccount")) { balance = chequing.getBalance();}
+	    if (accountType.contains("SavingsAccount")) { balance = savings.getBalance();}
 	    customer_balance = new Label("");
 	    customer_balance.setText("Your balance is " + balance);
 	    withdraw_button = new Button("Withdraw");
@@ -161,69 +158,18 @@ public class BankAccountApp extends Application {
 	    layout3.add(customer_balance, 1, 4);
 	    layout3.add(error, 1, 5);
 	    
-	    // Create button for SavingsAccount
-	    create_sav_acc.setOnAction(new EventHandler<ActionEvent>(){
-	      @Override
-	      public void handle(ActionEvent event) {
-	    	  	accountType = "SavingsAccount";
-		    	acctType_label = new Label(""+accountType+"");
-		    	layout2.add(acctType_label, 0, 0);
-		    	layout4.add(acctType_label, 0, 0);
-	    	  	Scene scene2 = new Scene(layout2, 400, 400);
-	    	  	primaryStage.setScene(scene2);
-	      }});
-	    
-	    // Create button for ChequingAccount
-	    create_chq_acc.setOnAction(new EventHandler<ActionEvent>(){
-	      @Override
-	      public void handle(ActionEvent event) {
-	    	accountType = "ChequingAccount";
-	    	acctType_label = new Label(""+accountType+"");
-	    	layout2.add(acctType_label, 0, 0);
-	    	layout4.add(acctType_label, 0, 0);
-    	  	Scene scene2 = new Scene(layout2, 400, 400);
-    	  	primaryStage.setScene(scene2);
-	      }});
-	    
-	    // Submit button for ChequingAccount & SavingsAccount
-	    submit.setOnAction(new EventHandler<ActionEvent>()
-	    {
-	      @Override
-	      public void handle(ActionEvent event) {
-    	  	cust_name = txtName.getText();
-    	  	customer = new Customer(cust_name, customer_ID);
-    	  	customer_label.setText("Hello " + customer.getName() + " " + customer.getID());
-    	  	
-    	  	if (accountType.contains("ChequingAccount")) {
-    	  		chequing.setCustomer(customer);
-    	    }
-    	    if (accountType.contains("SavingsAccount")) {
-    	    	savings.setCustomer(customer);
-    	    }
-    	  	
-    	  	acctType_label = new Label(""+accountType+"");
-        	layout3.add(acctType_label, 0, 0);
-    	  	Scene scene4 = new Scene(layout4, 400, 400);
-    	  	primaryStage.setScene(scene4);
-	      }});	    
-	    
-	    
-	    // For the last Screen when file does not exists.
+	 // For the last Screen when file does not exists.
 	    layout4 = new GridPane();
 	    layout4.setVgap(10);
 	    layout4.setHgap(5);
-	    
 	    if (accountType.contains("ChequingAccount")) {
 	    	balance = chequing.getBalance();
 	    	customer_balance = new Label("");
-	    	customer_balance.setText("Your balance is " + balance);
-	    }
+	    	customer_balance.setText("Your balance is " + balance);}
 	    if (accountType.contains("SavingsAccount")) {
 	    	balance = savings.getBalance();
 	    	customer_balance = new Label("");
-	    	customer_balance.setText("Your balance is " + balance);
-	    }
-	    
+	    	customer_balance.setText("Your balance is " + balance);}
 	    withdraw_button = new Button("Withdraw");
 	    deposit_button = new Button("Deposit");
 	    txtWithdraw = new TextField("0");
@@ -231,24 +177,21 @@ public class BankAccountApp extends Application {
 	    txtDeposit = new TextField("0");
 	    txtDeposit.setPrefWidth(100);
 	    error = new Label("");	    
-	    acctType_label = new Label(""+accountType+"");
 	    layout4.add(withdraw_button, 0, 2);
 	    layout4.add(deposit_button, 0,3);
-	    layout4.add(acctType_label, 0, 0);
 	    layout4.add(customer_label, 0, 1);
 	    layout4.add(txtWithdraw, 1, 2);
 	    layout4.add(txtDeposit, 1, 3);
 	    layout4.add(customer_balance, 1, 4);
 	    layout4.add(error, 1, 5);
-	       
-	    if (file_check()) {
 	    
-	    	//	Used by File Reader
+	    
+	    //	Used by File Reader
 	    	// For the last Screen when file exists.
-		    layout5 = new GridPane();
+	    if (file_check()) {
+	        layout5 = new GridPane();
 		    layout5.setVgap(10);
 		    layout5.setHgap(5);
-		    
 		    customer_balance = new Label("");
 		    customer_balance.setText("Your balance is " + balance);
 		    withdraw_button = new Button("Withdraw");
@@ -259,9 +202,7 @@ public class BankAccountApp extends Application {
 		    txtDeposit.setPrefWidth(100);
 		    error = new Label("");
 		    customer_label = new Label("Hello "+name+"  "+ID+"");
-		    
 		    acctType_label = new Label(""+accountType+"");
-		    
 		    layout5.add(withdraw_button, 0, 2);
 		    layout5.add(deposit_button, 0,3);
 		    layout5.add(acctType_label, 0, 0);
@@ -272,87 +213,103 @@ public class BankAccountApp extends Application {
 		    layout5.add(error, 1, 5);
 	    } 
 	    
-	    // Last screen 
-	    // Withdraw button for ChequingAccount & SavingsAccount
-	    //Uses layout3
+	    // Create button for SavingsAccount
+	    create_sav_acc.setOnAction(new EventHandler<ActionEvent>(){
+	      @Override
+	      public void handle(ActionEvent event) {
+	    	  	accountType = "SavingsAccount";
+		    	acctType_label = new Label(""+accountType+"");
+		    	layout2.add(acctType_label, 0, 0);
+		    	Scene scene2 = new Scene(layout2, 400, 400);
+	    	  	primaryStage.setScene(scene2);
+	      }});
+	    
+	    // Create button for ChequingAccount
+	    create_chq_acc.setOnAction(new EventHandler<ActionEvent>(){
+	      @Override
+	      public void handle(ActionEvent event) {
+	    	accountType = "ChequingAccount";
+	    	acctType_label = new Label(""+accountType+"");
+	    	layout2.add(acctType_label, 0, 0);
+    	  	Scene scene2 = new Scene(layout2, 400, 400);
+    	  	primaryStage.setScene(scene2);
+	      }});
+	    
+	    // Submit button for ChequingAccount & SavingsAccount
+	    submit.setOnAction(new EventHandler<ActionEvent>(){
+	      @Override
+	      public void handle(ActionEvent event) {
+    	  	cust_name = txtName.getText();
+    	  	customer = new Customer(cust_name, customer_ID);
+    	  	customer_label.setText("Hello " + customer.getName() + " " + customer.getID());
+    	  	if (accountType.contains("ChequingAccount")) { chequing.setCustomer(customer);}
+    	    if (accountType.contains("SavingsAccount")) { savings.setCustomer(customer);}
+    	    acctType_label = new Label(""+accountType+"");
+        	layout4.add(acctType_label, 0, 0);
+    	  	Scene scene4 = new Scene(layout4, 400, 400);
+    	  	primaryStage.setScene(scene4);
+	      }});	    
+	    
+	    // Withdraw button for ChequingAccount & SavingsAccount (Uses layout3)
 	    withdraw_button.setOnAction(new EventHandler<ActionEvent>(){
 	      @Override
 	      public void handle(ActionEvent event) {
     	  	  try {
-    	  		  
 	    	  	double withdraw_amount = Double.parseDouble(txtWithdraw.getText());
 	    	  	if (accountType.contains("ChequingAccount")){
 	    	  		chequing.withdraw(withdraw_amount);
-		    	  	customer_balance.setText("Your balance is "+ chequing.getBalance());
-
-		    	  	//write_file();
-	    	    }
+		    	  	customer_balance.setText("Your balance is "+ chequing.getBalance());}
 	    	    if (accountType.contains("SavingsAccount")){
 	    	    	savings.withdraw(withdraw_amount);
-	    	    	customer_balance.setText("Your balance is "+ savings.getBalance());
-	    	    	
-	    	    	//write_file();
-	    	    }
-	    		 
-	    	  	  error.setText("");
-    	  	  }
+	    	    	customer_balance.setText("Your balance is "+ savings.getBalance());}
+	    	    if (file_check()) {
+	    	    	write_file(); }
+	    	  	  error.setText(""); }
     	  	  catch (NumberFormatException e) {
-    	  		  error.setText("Error, you must input a number. ");
-    	  	  }
+    	  		  error.setText("Error, you must input a number. "); }
 	      }});
 	    
-	    // Last screen 
+	   
 	    // Deposit button for ChequingAccount & SavingsAccount
-	    //Uses layout3
-	    deposit_button.setOnAction(new EventHandler<ActionEvent>()
-	    {
+	    deposit_button.setOnAction(new EventHandler<ActionEvent>(){
 	      @Override
 	      public void handle(ActionEvent event) {
-	    	  
-    	  	  try {
+	    	  try {
 	    	  	  double deposit_amount = Double.parseDouble(txtDeposit.getText());
 	    	  	if (accountType.contains("ChequingAccount")){
-	    	  		
 	    	  		chequing.deposit(deposit_amount);
-	    	  		double bal_1 = chequing.getBalance();
-		    	  	customer_balance.setText("Your balance is "+ bal_1);
-		    	  	
-		    	  	//write_file();
-	    	    }
+	    	  		balance = chequing.getBalance();
+		    	  	customer_balance.setText("Your balance is "+ balance);}
 	    	    if (accountType.contains("SavingsAccount")){
 	    	    	savings.deposit(deposit_amount);
-	    	    	double bal_1 = savings.getBalance();
-	    	    	customer_balance.setText("Your balance is "+ bal_1);
-	    	    	
-	    	    	//write_file();
-	    	    }
+	    	    	balance = savings.getBalance();
+	    	    	customer_balance.setText("Your balance is "+ balance);}
+	    	    if (file_check()) {
+	    	    	write_file();}
 	    	  	  error.setText("");
-	    	  	  
-    	  	  }
-    	  	  catch (NumberFormatException e) {
-    	  		  error.setText("Error, you must input a number. ");
-    	  	  }
+		  	  }
+		  	  catch (NumberFormatException e) {
+		  		  error.setText("Error, you must input a number. ");
+		  	  }
 	      }});
-	 	
-	   
+	 		   
 	 	if (file_check()) {
 	 		Scene scene5 = new Scene(layout5, 450, 450);
 	 		primaryStage.setScene(scene5);
-	 		primaryStage.show();
-	 	}
+	 		primaryStage.show();}
 	 	else {
 	 		Scene scene = new Scene(root, 450, 150);
 		 	primaryStage.setTitle("Group3 Financial Bank");
 		 	primaryStage.setScene(scene);
-	 		primaryStage.show();
-	 	}    
-	}
+	 		primaryStage.show(); }   
+	 	}
 	
-	// if file does not exist
+	// if file does not exist this writes a new file 
 	public static void write_file(){
-
-		String name = txtName.getText();
-		
+		if (!file_check()) {
+			name = txtName.getText();
+		}
+			
 		try {
 			FileWriter file_w = new FileWriter(file_name);
 			BufferedWriter file_in = new BufferedWriter(file_w);
@@ -361,14 +318,15 @@ public class BankAccountApp extends Application {
 			file_in.write(accountType); file_in.newLine();
 			file_in.write(name); file_in.newLine();
 			String id = Integer.toString(customer.getID());
-			System.out.print(id);
+			//System.out.print(id);
 			file_in.write(id); file_in.newLine();
+			
 			if (accountType.contains("ChequingAccount")){
-	    	  	String bal = Double.toString(chequing.getBalance());
+	    	  	bal = Double.toString(chequing.getBalance());
 	    	  	file_in.write(bal);
     	    }
     	    if (accountType.contains("SavingsAccount")){
-    	    	String bal = Double.toString(savings.getBalance());
+    	    	bal = Double.toString(savings.getBalance());
     		    file_in.write(bal);
     	    }
 						
@@ -377,7 +335,11 @@ public class BankAccountApp extends Application {
 			file_in.close();
 		
 		} catch (IOException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
+			System.out.println("Somthing is Wrong: Diagnose");
+		} catch (Exception e) {
+			//e.printStackTrace();
+			System.out.println("Somthing is Wrong: Diagnose");
 		}
 	}
 	
@@ -385,6 +347,7 @@ public class BankAccountApp extends Application {
 		
 		if (file_check()) {
 		read_file();
+		write_file();
 		launch(args);
 		}
 		else {
