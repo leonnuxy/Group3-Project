abc351bdb6edd76202ebbaffe97db21a7e6ecf65
Mattt-K <35709212+Mattt-K@users.noreply.github.com>
2018-04-07 20:02:21 -0600

    Add files via upload

---
+++ Score.java
@@ -0,0 +1,19 @@
+import javafx.scene.control.Label;
+
+public class Score {
+	private static int score = 0;
+	
+	public static void setScore(int toIncrementBy) {
+		score += toIncrementBy;
+	}
+	
+	public static int getScore() {
+		return score;
+	}
+	
+	public static Label scoreLabel() {
+		Label score_label = new Label("Collectibles: " + getScore());
+		return score_label;
+	}
+	
+}
