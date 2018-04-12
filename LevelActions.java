fc827f545eeb016e4aa0f962069347e92f9ca4bd
nolaptopnolife <36118607+nolaptopnolife@users.noreply.github.com>
2018-04-11 20:13:21 -0600

    Update LevelActions.java

--- LevelActions.java
+++ LevelActions.java
@@ -1,6 +1,6 @@
-
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
@@ -8,7 +8,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public abstract class LevelActions extends Application {
-	
+
 	protected static final int TILE_SIZE = 10;
 	protected static final double PLAYER_SIZE = 9.99;
 	protected static final int APP_W = 800;
@@ -22,16 +22,18 @@ public abstract class LevelActions extends Application {
 	protected double difficulty = Difficulty.getDifficulty();
 	protected Timeline timeline = new Timeline();
 	protected ObservableList<Node> snake;
+	protected ObservableList<Node> snake2;
 	protected Stage primaryStage = new Stage();
 	protected Stage primaryStage2 = new Stage();
-	Rectangle head = new Rectangle(PLAYER_SIZE, PLAYER_SIZE);
+	protected static Rectangle head;
+	protected static Rectangle head2;
 	protected static boolean twoplayermode = false;
-	protected ObservableList<Node> snake2;
-	protected Direction direction2 = Direction.DOWN;
+	protected Direction direction2;
 	protected boolean moved2 = false;
-	protected int scoreChange = 1;
+	protected int scoreChange = 2;
 	protected boolean toRemove2;
 	public static boolean endGame = false;
+	protected Snake snakeIns = new Snake();
 
 	public LevelActions() {
 		super();
@@ -41,38 +43,43 @@ public abstract class LevelActions extends Application {
 		stopGame();
 		startGame();
 	}
-	
+
 	public void stopGame() {
 		running = false;
 		timeline.stop();
 		snake.clear();
 		snake2.clear();
-		//System.out.println("Time Elapsed: " + TimerS.getTotalTime());
 	}
-	
+
 	/*
 	 * puts the snake back to the top left of the screen to start again
 	 */
 	public void startGame() {
+		if (EndGame.restart) {
+			snake.clear();
+			snake2.clear();
+		}
+		
+		
 		direction = Direction.RIGHT;
 		direction2 = Direction.DOWN;
-		Rectangle head2 = new Rectangle(PLAYER_SIZE, PLAYER_SIZE);
+		head = Snake.getHead();
+		head2 = Snake.getHead();
 		head.setFill(Color.rgb(241, 249, 12));
 		head2.setFill(Color.rgb(0, 0, 0));
 		head2.setLayoutY(40);
 		snake.add(head);
 		snake2.add(head2);
 		endGame = false;
-		twoplayermode = false;
 		timeline.play();
-		running = true;	
+		running = true;
 	}
-	//public abstract Scene run();
-	
+
 	public void endGame() {
 		timeline.stop();
 		primaryStage.close();
 		System.out.println("You win.");
+		twoplayermode = false;
 		EndGame end = new EndGame();
 		try {
 			end.start(primaryStage);
