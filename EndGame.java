6f14b03daed75678d0737fe1391e0fd6600b49e8
nolaptopnolife <36118607+nolaptopnolife@users.noreply.github.com>
2018-04-12 00:43:00 -0600

    Update EndGame.java

--- EndGame.java
+++ EndGame.java
@@ -1,61 +1,99 @@
 import javafx.application.Application;
+import javafx.scene.layout.Pane;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.scene.paint.Color;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
 
 /**
- * End game screen for a game of snake that displays score and gives a restart option
- * @author MyPrecious
- *
- */
+* Screen EndGame will appear when player finishes the game/dies. Give the option
+to play again or to quit. Displays score from game played
+*/
 public class EndGame extends Application{
 
-	//boolean for restarting some key aspects in LevelActions Class
 	public static boolean restart;
 
-	
-	/*
-	 * This method runs the end game screen
-	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		//Create the pan to put things on
-		Pane root = new Pane();
+		TimerS.setEndTime(); //End time of current game
 		
-		//Create the buttons
-		Button butt = new Button("Play Again");
-		Button butt2 = new Button("Quit");
-		// set button positions
-		butt.setLayoutX(100);
-		butt.setLayoutY(150);
-		butt2.setLayoutX(200);
-		butt2.setLayoutY(150);
+		/* Creation of the EndGame screen */
+		Pane endGamePane = new Pane();
+		Scene endGameScene = new Scene(endGamePane, 800 ,800);
 		
-		//set the end time when the end game screen is triggered
-		TimerS.setEndTime();
-		Label lab = new Label("Your Final Score is "+ Score.getScore() * TimerS.getTotalTime());
-		lab.setLayoutX(10);
-		lab.setLayoutY(200/2);
-		//add the buttons and labels to the screen
-		root.getChildren().addAll(butt, butt2, lab);
-		Scene newScene = new Scene(root, 300,300, Color.RED);
-		primaryStage.setScene(newScene);
+		/* Creation of buttons and labels */
+		Button playAgain = new Button("Play Again?");
+		Button quit = new Button("Quit");
+		Label deathLabel = new Label("You Died");
+		
+		//Creation of score which is the time survived multiplied by collectibles collected
+		Label scoreLabel = new Label("Your Final Score is "+ Score.getScore() * TimerS.getTotalTime());
+		
+		/* EndGame BGM file usage. Creation of Media and playing of sound file */
+		String diedSoundFile = "diedSound.mp3";     
+		Media diedSoundMedia = new Media(new File(diedSoundFile).toURI().toString());
+		MediaPlayer diedSoundPlayer = new MediaPlayer(diedSoundMedia);
+		diedSoundPlayer.play();
+		
+		/* ButtonSound file usage. Creation of Media using the sound file */
+		String buttonSoundFile = "buttonSound.mp3";     
+		Media buttonSoundMedia = new Media(new File(buttonSoundFile).toURI().toString());
+		
+		/* Set size and position for the playAgain button */
+		playAgain.setLayoutX(225);
+		playAgain.setLayoutY(400);
+		playAgain.setPrefSize(350, 50); 
+		
+		/* Set size and position for the quit button */
+		quit.setLayoutX(225);
+		quit.setLayoutY(500);
+		quit.setPrefSize(350, 50); 
+		
+		/* Set size and position for the deathLabel label */
+		deathLabel.setLayoutX(250);
+		deathLabel.setLayoutY(200);
+		deathLabel.setFont(new Font(70));
+		
+		/* Set size and position for the scoreLabel label */
+		scoreLabel.setLayoutX(300);
+		scoreLabel.setLayoutY(350);
+		scoreLabel.setFont(new Font(20));
+		
+		/* Set font size and colour for buttons in menu */
+        playAgain.setStyle("-fx-font-size: 4em;-fx-background-color: #4682b4 ");
+		quit.setStyle("-fx-font-size: 4em;-fx-background-color: #ffff00  ");
+
+		/* Add buttons and labels to settings screen */
+		endGamePane.getChildren().addAll(playAgain, quit, deathLabel, scoreLabel);
+		
+		/* Set primaryStage scene to the endGame screen */
+		primaryStage.setScene(endGameScene);
 		primaryStage.show();
 		
-		//set an action for the button to restart from the main menu
-		butt.setOnAction(e -> {
+		/* Actions for playAgain button which changes the scene to the initial menu
+		screen */
+		playAgain.setOnAction(e -> {
 			restart = true;
 			primaryStage.close();
 			Menu menu = new Menu();
 			menu.start(primaryStage);
+			
+			// button sound played
+			MediaPlayer buttonSoundPlayer = new MediaPlayer(buttonSoundMedia);
+			buttonSoundPlayer.play();
+			diedSoundPlayer.stop(); // stop endGame BGM
+			
 		});
 		
-		//exit button that ends the program
-		butt2.setOnAction(e -> {
+		/* Actions for quit button which exits the application */
+		quit.setOnAction(e -> {
 			System.exit(0);
 		});
 		
